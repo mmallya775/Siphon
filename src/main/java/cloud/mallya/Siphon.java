88432e86e4107011650e80b41a95fe2d7b2de980
Mohit Radhesh Mallya <mmallya775@gmail.com>
2026-05-08 22:33:30 +0200

    Feat: Implement local folder scanning along with its metadata
    The metadata can be used to compare with the metadata of the destination folder and differentiate based on last modified time and its size whether the file needs syncing or not.

--- src/main/java/cloud/mallya/Siphon.java
+++ src/main/java/cloud/mallya/Siphon.java
@@ -3,14 +3,23 @@ package cloud.mallya;
 import cloud.mallya.model.FileMeta;
 import cloud.mallya.scan.LocalScanner;
 
+import java.nio.file.Path;
 import java.util.Map;
 
 public class Siphon {
     static void main() {
-        LocalScanner localDirectoryUtilities = new LocalScanner();
 
-        Map<String, FileMeta> listOfFiles = localDirectoryUtilities.listLocal();
+        Path localFolder = Path.of("/home/mmallya/Documents/RustProjects/hello_world");
+        LocalScanner localDirectoryUtilities = new LocalScanner(localFolder);
 
+        Map<String, FileMeta> listOfFiles = localDirectoryUtilities.scan();
         System.out.println(listOfFiles);
+
+//        listOfFiles.entrySet().stream()
+//                .sorted(Map.Entry.comparingByKey())
+//                .forEach(e -> System.out.printf("%-50s %100d bytes  %s%n",
+//                        e.getKey(),
+//                        e.getValue().size(),
+//                        Instant.ofEpochMilli(e.getValue().timestamp())));
     }
 }
--- src/main/java/cloud/mallya/scan/LocalScanner.java
+++ src/main/java/cloud/mallya/scan/LocalScanner.java
@@ -2,11 +2,47 @@ package cloud.mallya.scan;
 
 import cloud.mallya.model.FileMeta;
 
+import java.io.IOException;
+import java.io.UncheckedIOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.util.HashMap;
 import java.util.Map;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import java.util.stream.Stream;
 
 public class LocalScanner {
-    public Map<String, FileMeta> listLocal() {
 
-        return Map.of();
+    private final Path localFolderPath;
+    private static final Logger log = Logger.getLogger(LocalScanner.class.getName());
+
+    public LocalScanner(Path localFolderPath) {
+        this.localFolderPath = localFolderPath;
+    }
+
+    public Map<String, FileMeta> scan() {
+
+        Map<String, FileMeta> scannedList = new HashMap<>();
+
+        try (Stream<Path> localFileStream = Files.walk(localFolderPath)) {
+            localFileStream
+                    .filter(path -> !Files.isDirectory(path))
+                    .forEach(path -> {
+                        try {
+                            String key = localFolderPath.relativize(path).toString();
+                            BasicFileAttributes basicFileAttributes = Files.readAttributes(path, BasicFileAttributes.class);
+                            scannedList.put(key, new FileMeta(basicFileAttributes.size(), basicFileAttributes.lastModifiedTime().toMillis()));
+                        } catch (IOException e) {
+                            log.log(Level.WARNING, e, () -> "Failed to fetch file attributes for: " + path);
+                        }
+                    });
+
+        } catch (IOException e) {
+            log.log(Level.SEVERE, e, () -> "Failed to scan folder: " + localFolderPath);
+            throw new UncheckedIOException("Failed to scan " + localFolderPath, e);
+        }
+        return scannedList;
     }
 }
