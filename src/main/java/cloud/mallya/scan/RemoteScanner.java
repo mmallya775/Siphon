25bef8eb6f95fa303b010cd444efb34162aedb27
Mohit Radhesh Mallya <mmallya775@gmail.com>
2026-05-10 22:00:18 +0200

    Doc: Documented the rationale behind using server side `find` over SftpFileSystem based filewalker

--- src/main/java/cloud/mallya/scan/RemoteScanner.java
+++ src/main/java/cloud/mallya/scan/RemoteScanner.java
@@ -7,6 +7,15 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.logging.Logger;
 
+/**
+ * Using the SftpFileSystem to list out the files and its arttributes is very slow
+ * for large directories with multiple nested subdirectories. We could try to list out the
+ * files recursively in the server itself and send its results over to the client via
+ * the clientSesion.createExecChannel(command). The result is an extremely fast listing of
+ * files in the remote directory. I am currently only aiming for the case where the remote
+ * system is also UNIX-based. Not sure if other operating systems can utilize the `find` command.
+ */
+
 public class RemoteScanner {
 
     private final Path remoteFolderPath;
